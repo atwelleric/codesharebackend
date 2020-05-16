c7e84929880c84adb01590f90e2bd7ee4422e26a
wrightw <wrightw2055@gmail.com>
2020-05-16 17:37:35 -0400

    Custom errors implemented

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const codeController = require('./controllers/codes');
+const {handleErrors} = require('./middleware/custom_errors')
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', codeController);
 
+app.use((err, req, res, next) => {
+	const statuscode = err.statuscode || 500;
+	const message = err.message || 'Internal Server Error';
+	res.status(statuscode).send(message);
+});
+
 app.set('port', process.envPORT || 4000);
+
 
 app.listen(app.get('port'), () => {
 	console.log('listening on port' + app.get('port'));
---
+++ middleware/custom_errors.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose');
+
+class OwnershipError extends Error {
+	constructor() {
+		super();
+		this.name = 'OwnershipError';
+		this.statusCode = 401;
+		this.message = 'You must be tired? Because you messed up on the token';
+	}
+}
+
+class DocumentNotFoundError extends Error {
+	constructor() {
+		super();
+		this.name = 'DocumentNotFoundError';
+		this.statusCode = 404;
+		this.message = 'You messed up on the ID, try again';
+	}
+}
+
+class BadParamsError extends Error {
+	constructor() {
+		super();
+		this.name = 'BadParamsError';
+		this.statusCode = 422;
+		this.message = 'How you mess up on params?';
+	}
+}
+
+class BadCredentialsError extends Error {
+	constructor() {
+		super();
+		this.name = 'BadCredentialsError';
+		this.statusCode = 422;
+		this.message = 'Wrong username and password? You trippin';
+	}
+}
+
+class InvalidIdError extends Error {
+	constructor() {
+		super();
+		this.name = 'InvalidIdError';
+		this.statusCode = 422;
+		this.message = 'Wrong Id bro';
+	}
+}
+
+
+const handleValidateOwnership = (requestObject, resource) => {
+	if (!requestObject.user._id.equals(resource.owner)) {
+		throw new OwnershipError();
+	} else {
+		return record;
+	}
+};
+
+const handleRecordExists = (record) => {
+	if (!record) {
+		throw new DocumentNotFoundError();
+	} else {
+		return record;
+	}
+};
+
+const handleValidateId = (req, res, next) => {
+	const isValidId = mongoose.Types.ObjectId.isValid(req.params.id);
+	if (!isValidId) {
+		throw new InvalidIdError();
+	} else {
+		next();
+	}
+};
+
+const handleValidateErrors = (err, req, res, next) => {
+	if (err.name.match(/Valid/) || err.name === 'MongoError') {
+	} else {
+		next(err);
+	}
+};
+
+const handleErrors = (err, req, res, next) => {
+	const statusCode = err.statusCode || 500;
+	const message = err.message || 'Internal Server Error';
+	res.status(statusCode).send(message);
+};
+
+
+module.exports = {
+	handleValidateOwnership,
+	handleRecordExists,
+	handleValidateId,
+	handleValidateErrors,
+	handleErrors,
+};
