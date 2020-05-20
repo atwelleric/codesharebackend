7fe3b3648a73b236cb4b445c3511b719fc1d1a92
atwelleric <atwelleric@gmail.com>
2020-05-20 00:40:39 -0500

    still trying to deploy

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const codeController = require('./controllers/codes');
 const userController = require('./controllers/users');
 const { handleErrors } = require('./middleware/custom_errors');
 
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,8 +18,7 @@ app.use((err, req, res, next) => {
 	res.status(statuscode).send(message);
 });
 
-app.set('port', process.envPORT || 4000);
-
+app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), () => {
-	console.log('listening on port' + app.get('port'));
+	console.log(`✅ PORT: ${app.get('port')} 🌟`);
 });
